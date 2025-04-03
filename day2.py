9b341446ff5af5c0991fe7bf594aa7e6ec52b86b
EnabothulaSreeja <140228555+EnabothulaSreeja@users.noreply.github.com>
2025-04-03 21:46:32 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,22 @@
+print('welcome')
+#name='sreeja'
+n1=input('enter :')
+print('my name',n1)
+
+name=input('enter name:')
+age=input('enter age:')
+address=input('enter address:')
+print('My details')
+print('my name is',name)
+print('my age is',age)
+print('my address is',address)
+
+print('hello','revature',sep='-',end='!')
+
+n1=int(input('enter:'))
+n2=int(input('enter:'))
+n3=n1+n2
+print(n3)
+print(type(n3))
+n='hi friends'
+print(type(n))
